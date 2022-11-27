a83527866acdb7dc98205700e5e3b1fc26f0a5dd
musa-f <musaevafatima77@gmail.com>
2022-11-27 12:56:58 +0100

    btn click function

--- script.js
+++ script.js
@@ -1,13 +1,23 @@
-let btnReset = document.getElementById('reset');
+/*let btnReset = document.getElementById('reset');
+btnReset.addEventListener('click', askLetter);*/
 
-btnReset.addEventListener('click', askLetter);
+function clickBtn(){
+    let input = document.getElementById('input');
+    let btnKeys = document.querySelectorAll('button.key')
 
-let btnKey = document.getElementById('key');
+    btnKeys.forEach(function(btnKey){
+        btnKey.addEventListener('click', function(){
+            letterKey = btnKey.textContent;
+            input.append(letterKey);
+        })
+    })
+}
 
 
 function askLetter(){
     let arrayWord = ['chat', 'chien', 'lapin', 'oiseau', 'poussin',"tortue"];
     let randomWord = arrayWord[Math.floor(Math.random()*arrayWord.length)]; 
+    console.log(randomWord)
     let randomWordArray = randomWord.split("");
 
     let letter; 
@@ -25,11 +35,15 @@ function askLetter(){
                     console.log(word);
                 }
             }
-        }else{
-            compteur ++;
-        }
-        if (compteur == 5){
+        } 
+        else if(compteur == 3){
             word = []
+            compteur = 0;
+        }
+        else{
+            compteur ++;
         }
     } while(word.join('').toString() !== randomWord);
 }
+
+askLetter()
